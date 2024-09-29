d4019dc170c066d84c41194545d73e196af99e7b
zoomzoomzee <z.zcong677@gmail.com>
2024-09-29 06:05:48 -0500

    turned off taro-ui prebundle for webpack

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,5 +1,6 @@
 import { View, Text } from '@tarojs/components'
 import { useLoad } from '@tarojs/taro'
+import {AtButton, AtCalendar} from 'taro-ui'
 import './index.scss'
 
 export default function Index() {
@@ -11,6 +12,14 @@ export default function Index() {
   return (
     <View className='index'>
       <Text>Hello world!</Text>
+      <AtButton type='primary'>提交</AtButton>
+      <AtCalendar
+        isSwiper
+        isMultiSelect currentDate={{start: '2024/9/22', end: '2024/9/28'}}
+        minDate="2024/9/1" maxDate="2024/9/30"
+        marks={[ { value: '2024/9/27' } ]}
+        hideArrow="false"
+      />
     </View>
   )
 }
